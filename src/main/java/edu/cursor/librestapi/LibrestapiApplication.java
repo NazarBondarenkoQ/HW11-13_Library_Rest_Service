2b4b943da41aa7b12cdfd4e9d4ede3c2fb925da9
NazarBondarenkoQ <nazarbondarenko1@gmail.com>
2019-08-06 09:53:10 +0300

    Initial commit.

---
+++ src/main/java/edu/cursor/librestapi/LibrestapiApplication.java
@@ -0,0 +1,13 @@
+package edu.cursor.librestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrestapiApplication.class, args);
+    }
+
+}
